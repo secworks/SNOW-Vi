cd0a454a520598239f8258c0578078faa3e5d1ac
Joachim Strömbergson <joachim@assured.se>
2024-04-12 17:05:17 +0200

    Adding initial functionality for the ref model
    Start of context struct
    Function prototypes
    Test keys and IVs
    An hello world test program
    
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

---
+++ src/model/reference/snow_vi.h
@@ -0,0 +1,62 @@
+//=======================================================================
+// snow_vi.h
+// ---------
+// Header with definition of context struct and function prototype for
+// the SNOW-Vi reference model that tries to follow the definition
+// in the paper to the letter.
+//
+//
+// Author: Joachim Strömbergon
+// Copyright 2024 Assured AB
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+// CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//=======================================================================
+
+#include <stdint.h>
+
+struct snow_vi_ctx {
+  uint16_t lfsr_a[8];
+  uint16_t lfsr_b[8];
+};
+
+// Given a key  an iv, return  an initialized snow_vi context.
+struct snow_vi_ctx* snow_vi_init(uint8_t *key, uint8_t *iv);
+
+// Update the state one step.
+void snow_vi_update(struct snow_vi_ctx *ctx);
+
+// Print the generated keystream for the current given state.
+void snow_vi_print_keystream(struct snow_vi_ctx *ctx);
+
+// Print the current given state.
+void snow_vi_print_state(struct snow_vi_ctx *ctx);
+
+
+//=======================================================================
+// EOF snow_vi.h
+//=======================================================================
---
+++ src/model/reference/snow_vi_test.c
@@ -0,0 +1,82 @@
+//=======================================================================
+// snow_vi_test.c
+// --------------
+// Test program for snow_vi. It use a set of keys and IVs to test
+// the functionality of the reference model.
+//
+//
+// Author: Joachim Strömbergon
+// Copyright 2024 Assured AB
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+// CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+// AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//=======================================================================
+
+#include <stdio.h>
+#include "snow_vi.h"
+
+// Test keys and IVs
+const uint8_t key[][32] = {
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+
+    { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+
+    { 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+        0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f,
+        0x0a, 0x1a, 0x2a, 0x3a, 0x4a, 0x5a, 0x6a, 0x7a,
+        0x8a, 0x9a, 0xaa, 0xba, 0xca, 0xda, 0xea, 0xfa }
+};
+
+const uint8_t iv[][16] = {
+    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },
+
+    { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+
+    { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+        0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10 }
+};
+
+
+int main(void) {
+  printf("snow_vi test started.\n");
+
+  printf("snow_vi test completed.\n");
+
+  return 0;
+}
+
+//=======================================================================
+// EOF snow_vi_test.c
+//=======================================================================
